69f968db4439ec73916d32dfc817916514120e46
Christian Guidikov <christian.guidikov@gmail.com>
2022-11-05 01:07:29 +0200

    first iteration of the frontend

--- scripts/alternatives.js
+++ scripts/alternatives.js
@@ -1,7 +1,28 @@
 const firstDiv = document.getElementsByClassName("ProductDetails ImgLeft");
 
 if (firstDiv) {
-    firstDiv.item(0).setAttribute("style", "background:red;")
-    const suggestionElement = document.createElement("div");
+    const addedDiv = document.createElement("div");
+    firstDiv.item(0).appendChild(addedDiv);
+    addedDiv.setAttribute('style', 'height: 150px; margin-top: 20px; background: red;');
+    
+    const firstElem = document.createElement("div");
+    const firstImg = document.createElement("img");
 
+    const secondElem = document.createElement("div");
+    const secongImg = document.createElement("img");
+
+    const thirdElem = document.createElement("div");
+    const thirdImg = document.createElement("img");
+
+    const suggestionsArray = new Array(firstElem, secondElem, thirdElem);
+    const imgArray = new Array(firstImg, secongImg, thirdImg);
+    
+
+    for (let i = 0; i < suggestionsArray.length; i++) {
+        addedDiv.appendChild(suggestionsArray[i]);
+        suggestionsArray[i].appendChild(imgArray[i])
+        suggestionsArray[i].setAttribute('style', 'height: 50px; background: green;');
+        suggestionsArray[i].innerHTML = 'hey';
+    }
+    
 }
