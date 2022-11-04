35255598d60f4832d01486aea1490b61bb828629
hedgeho <ilya.zalessky@yandex.ru>
2022-11-05 00:20:30 +0200

    initial

---
+++ scripts/alternatives.js
@@ -0,0 +1,7 @@
+const firstDiv = document.getElementsByClassName("ProductDetails ImgLeft");
+
+if (firstDiv) {
+    firstDiv.item(0).setAttribute("style", "background:red;")
+    const suggestionElement = document.createElement("div");
+
+}
