a70007d13b42c8d0d5f7c8368aea8be1dc000416
Christian Guidikov <christian.guidikov@gmail.com>
2022-11-05 16:03:02 +0200

    modify the alternatives

--- scripts/alternatives.js
+++ scripts/alternatives.js
@@ -9,7 +9,7 @@ if (firstDiv) {
     const susAltText = document.createElement("div");
     firstDiv.item(0).appendChild(susAltText);
     susAltText.setAttribute('style', 'margin-top: 30px; font-family: sans-serif; font-size: 2em');
-    susAltText.innerHTML = 'Sustaible alternatives';
+    susAltText.innerHTML = 'Sustainable alternatives';
     // -------------------------------------------------
 
 
@@ -34,89 +34,127 @@ if (firstDiv) {
 
     addedDiv.setAttribute('style', `height: ${suggestionsSize} px; margin: 10px 0px 0px 0px; display: flex; flex-direction: row;`);
 
-    const name = 'steel shit'
+    const name = 'Kuumavalssattu%201D%20RST%20levy%2012.0x1500x3000mm'
     fetch(`${HOST}/get_info?name=${name}`, {method: "GET"})
         .then(res => res.json())
         .then(res => {
+            const product_info = res[0]
             const co2Em = res[0]['Co2Em']
             const emissionsView = document.createElement("p")
             // todo multiply by amount
             emissionsView.innerText = "Total emissions: " + co2Em + " kg CO2e / kg"
+
+            fetch(`${HOST}/get_alternatives?name=${name}`, {method: "GET"})
+                .then(res => res.json())
+                .then(res => {
+                    const alternatives = res
+                    console.log(res)
+
+
+                    // ADDING SUGGESTIONS -------------------------------------------------------
+                    for (let i = 0; i < res.length; i++) {
+                        addedDiv.appendChild(suggestionsArray[i]); // To container for all 3 suggestions, add each suggestion div
+                        suggestionsArray[i].setAttribute('style', 'height: ' + imgSize + 'px; display: flex; flex-direction: row; overflow: hidden; margin-right: 10px;');
+
+                        const imgDiv = document.createElement("div"); // div for suggestion image
+                        const rightDiv = document.createElement("div"); // div for name, price, etc
+
+                        imgDiv.setAttribute('style', 'height: 100%;');
+                        rightDiv.setAttribute('style', 'display: flex; flex-direction: column;');
+
+                        suggestionsArray[i].appendChild(imgDiv);
+                        suggestionsArray[i].appendChild(rightDiv); // add the two divs to each suggestion
+
+                        imgDiv.appendChild(imgArray[i])
+                        imgArray[i].src = res[i]['ImageURL'];
+                        imgArray[i].setAttribute('style', 'padding: 5px 5px 5px 5px; max-height: 90px;');
+                
+
+
+
+
+                        // Description of the suggestion ----------------------------------------
+                        const namePriceDiv = document.createElement("div");
+                        const emissionsDiv = document.createElement("div");
+
+                        namePriceDiv.classList.add("namePrice");
+                        emissionsDiv.classList.add("emissions");
+
+                        rightDiv.appendChild(namePriceDiv);
+                        rightDiv.appendChild(emissionsDiv);
+
+                        namePriceDiv.setAttribute('style', 'height: ' + rightDiv.offsetHeight/2 + 'px; margin: 5px 5px 5px 5px;');
+                        namePriceDiv.innerHTML = res[i]['Name'];
+
+                        emissionsDiv.setAttribute('style', 'height: ' + rightDiv.offsetHeight/2 + 'px; margin: 0px 5px 5px 5px; text-align: center; display: flex; flex-direction: column;');
+                        const emissionsSpan = document.createElement("span");
+                        const priceSpan = document.createElement("span");
+                        emissionsDiv.appendChild(emissionsSpan);
+                        emissionsDiv.appendChild(priceSpan);
+                        emissionsSpan.setAttribute('style', 'background: lightgreen; display:inline-block;');
+                        emissionsSpan.innerHTML = `-${res[i]['Co2Em']} CO2e kg`;
+
+                        const productPrice = Number(product_info['Price'].slice(0,product_info['Price'].length-3));
+                        const altPrice = Number(res[i]['Price'].slice(0,res[i]['Price'].length-3));
+                        console.log("product price", productPrice)
+                        console.log("alt price", altPrice);
+
+
+                        if (productPrice > altPrice)
+                            priceSpan.innerHTML=`-${Math.round(Math.abs(altPrice-productPrice))}$`
+                        else 
+                            priceSpan.innerHTML=`+${Math.round(Math.abs(altPrice-productPrice))}$`
+
+                    }
+                    // --------------------------------------------------------------------------------
+                    
+
+
+
+
+                    // const tagNames = document.getElementsByClassName("ReferencePrice AlignLeft");
+
+                    // if (tagNames) {
+                    //     const tagsDiv = document.createElement("div");
+                    //     tagNames.item(0).appendChild(tagsDiv);
+                    //     tagsDiv.setAttribute('style', 'height: 50px; background: red; margin: 5px 0px 10px 0px; display: flex; flex-direction: row;');
+
+                    //     const tag1 = document.createElement("div");
+                    //     tag1.innerHTML = "Non-hazardous";
+
+                    //     const tag2 = document.createElement("div");
+                    //     tag2.innerHTML = "100% recyclable";
+
+                    //     const tags = [tag1, tag2];
+
+                    //     for (let i = 0; i < tags.length; i++) {
+                    //         tagsDiv.appendChild(tags[i]);
+                    //         tags[i].setAttribute('style', 'height: 40px; background: blue; margin: 5px 5px 5px 5px; vertical-align: middle;');
+                    //     }
+
+
+
+                    // }
+
+
+
+
+
+
+                    const sustainabilityInfo = document.getElementsByClassName("InfoArea New");
+
+                    if (sustainabilityInfo) {
+                        const susDiv = document.createElement("div");
+                        sustainabilityInfo[0].appendChild(susDiv)
+
+                        susDiv.setAttribute('style', 'background; red; height: 20px')
+                        susDiv.innerHTML = 'hey';
+
+
+                            
+                        }
+                })
         })
-    fetch(`${HOST}/get_alternatives?name=${name}`, {method: "GET"})
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            for (let i = 0; i < res.length; i++) {
-                addedDiv.appendChild(suggestionsArray[i]);
-                suggestionsArray[i].appendChild(imgArray[i])
-                suggestionsArray[i].setAttribute('style', 'height: 50px;');
-                suggestionsArray[i].innerHTML = res[i]['Name'];
-            }
-        })
-
-    for (let i = 0; i < suggestionsArray.length; i++) {
-        addedDiv.appendChild(suggestionsArray[i]); // To container for all 3 suggestions, add each suggestion div
-        suggestionsArray[i].setAttribute('style', 'height: ' + imgSize + 'px; background: green; display: flex; flex-direction: row; overflow: hidden; margin-right: 10px;');
-
-        const imgDiv = document.createElement("div"); // div for suggestion image
-        const rightDiv = document.createElement("div"); // div for name, price, etc
-
-        imgDiv.setAttribute('style', 'height: 100%; background: red;');
-        rightDiv.setAttribute('style', 'display: flex; flex-direction: column;');
-
-        suggestionsArray[i].appendChild(imgDiv);
-        suggestionsArray[i].appendChild(rightDiv); // add the two divs to each suggestion
-
-        imgDiv.appendChild(imgArray[i])
-        imgArray[i].src = 'http://www.precioussteel.com.ph/wp-content/uploads/2014/11/mild-steel-plates.jpg';
-        imgArray[i].setAttribute('style', 'padding: 5px 5px 5px 5px; max-height: 90px;');
-
-
-
-        // Description of the suggestion ----------------------------------------
-        const namePriceDiv = document.createElement("div");
-        const emissionsDiv = document.createElement("div");
-
-        namePriceDiv.classList.add("namePrice");
-        emissionsDiv.classList.add("emissions");
-
-        rightDiv.appendChild(namePriceDiv);
-        rightDiv.appendChild(emissionsDiv);
-
-        namePriceDiv.setAttribute('style', 'background: blue; width: 100%; height: ' + rightDiv.offsetHeight/2 + 'px; margin: 5px 5px 5px 5px;');
-        namePriceDiv.innerHTML = 'hey';
-
-        emissionsDiv.setAttribute('style', 'background: purple; width: 100%; height: ' + rightDiv.offsetHeight/2 + 'px; margin: 5px 5px 5px 5px;');
-        emissionsDiv.innerHTML = 'here are the emissions akjdhfljkahsdlfkjhalskjd';
-
-
-    }
     
-}
-
-
-
-const tagNames = document.getElementsByClassName("ReferencePrice AlignLeft");
-
-if (tagNames) {
-    const tagsDiv = document.createElement("div");
-    tagNames.item(0).appendChild(tagsDiv);
-    tagsDiv.setAttribute('style', 'height: 50px; background: red; margin: 5px 0px 10px 0px; display: flex; flex-direction: row;');
-
-    const tag1 = document.createElement("div");
-    tag1.innerHTML = "Non-hazardous";
-
-    const tag2 = document.createElement("div");
-    tag2.innerHTML = "100% recyclable";
-
-    const tags = [tag1, tag2];
-
-    for (let i = 0; i < tags.length; i++) {
-        tagsDiv.appendChild(tags[i]);
-        tags[i].setAttribute('style', 'height: 40px; background: blue; margin: 5px 5px 5px 5px; vertical-align: middle;');
-    }
-
-
 
 }
--- scripts/productListIcons.js
+++ scripts/productListIcons.js
@@ -1,16 +1,21 @@
 const infoArea = document.getElementsByClassName("InfoArea");
+const mediumColumn = document.getElementsByClassName("ListItemCategoryTable");
 
 if (infoArea) {
-    console.log(infoArea[0]);
-
+    
     for (let i=0; i < infoArea.length; i++) {
-        var style =infoArea[0].attr('style')
+        infoArea[i].style.display = "flex";
+        // infoArea[i].style.flexDirection = "row";
+        // infoArea[i].style.width = "auto";
+        // infoArea[i].childNodes[1].setAttribute('style', '');
+        const heightSus = infoArea[i].offsetHeight
 
-        infoArea[i].setAttribute('style', 'display: flex; flex-direction: row;');
+        infoArea[i].childNodes[1].style.display = "block"
+        infoArea[i].childNodes[1].style.width = "100%"
 
         const sustainabilityScore = document.createElement("div");
-        sustainabilityScore.setAttribute('style', 'background: red;');
-        sustainabilityScore.innerHTML = '106.5';
+        sustainabilityScore.setAttribute('style', `display: block; padding: ${heightSus/4}px; background: #102999;`);
+        sustainabilityScore.innerHTML = '106.5fasdfas';
 
         infoArea[i].appendChild(sustainabilityScore);
     }
