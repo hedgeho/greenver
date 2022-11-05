2fe9b0b4abe3d9c48014fb78f3ef15dd54071627
Christian Guidikov <christian.guidikov@gmail.com>
2022-11-05 19:24:38 +0200

    finish the product icons

--- scripts/productListIcons.js
+++ scripts/productListIcons.js
@@ -14,8 +14,18 @@ if (infoArea) {
         infoArea[i].childNodes[1].style.width = "100%"
 
         const sustainabilityScore = document.createElement("div");
-        sustainabilityScore.setAttribute('style', `display: block; padding: ${heightSus/4}px; background: #102999;`);
-        sustainabilityScore.innerHTML = '106.5fasdfas';
+        sustainabilityScore.setAttribute('style', `display: flex; flex-direction: row; padding: ${heightSus/4}px; background: #102999;`);
+        const susScore = document.createElement("div");
+        sustainabilityScore.appendChild(susScore);
+        susScore.setAttribute('style', 'color: white;')
+        susScore.innerHTML = "106.5"
+        
+        const susImage = document.createElement("img");
+        susImage.src = "https://insightarch.com/wp-content/uploads/2015/04/sustainability.png"
+        susImage.setAttribute('style', `max-height: ${heightSus/2}px; margin-left: 10px`)
+        sustainabilityScore.appendChild(susImage);
+        
+        // sustainabilityScore.innerHTML = '106.5fasdfas';
 
         infoArea[i].appendChild(sustainabilityScore);
     }
