b216012e8da9bef4ba60beeeb46a917e086291be
hedgeho <ilya.zalessky@yandex.ru>
2022-11-05 18:48:11 +0200

    stuff

--- scripts/alternatives.js
+++ scripts/alternatives.js
@@ -1,5 +1,7 @@
 const firstDiv = document.getElementsByClassName("ProductDetails ImgLeft");
 const standardSize = 100;
+const suggestionsSize = standardSize * 3; // by default: standardSize = 100px
+const imgSize = standardSize;
 
 
 HOST = 'http://10.84.110.213:5000'
@@ -13,29 +15,29 @@ if (firstDiv) {
     // -------------------------------------------------
 
 
+    const suggestionsDiv = document.createElement("div");
+    firstDiv.item(0).appendChild(suggestionsDiv);
 
-    const addedDiv = document.createElement("div");
-    firstDiv.item(0).appendChild(addedDiv);
+    // const firstSugg = document.createElement("div");
+    // const firstImg = document.createElement("img");
+    //
+    // const secondSugg = document.createElement("div");
+    // const secongImg = document.createElement("img");
+    //
+    // const thirdSugg = document.createElement("div");
+    // const thirdImg = document.createElement("img");
+    //
+    // const suggestionsArray = [firstSugg, secondSugg, thirdSugg]; // an array of all the suggestions
+    // const imgArray = [firstImg, secongImg, thirdImg]; // an array of all the images of suggestions
 
-    const firstSugg = document.createElement("div");
-    const firstImg = document.createElement("img");
 
-    const secondSugg = document.createElement("div");
-    const secongImg = document.createElement("img");
+    suggestionsDiv.setAttribute('style', `height: ${suggestionsSize} px; margin: 10px 0px 0px 0px; display: flex; flex-direction: row;`);
 
-    const thirdSugg = document.createElement("div");
-    const thirdImg = document.createElement("img");
+    const name = document.getElementsByClassName("InfoArea New")[0].children[0].textContent
 
-    const suggestionsArray = [firstSugg, secondSugg, thirdSugg]; // an array of all the suggestions
-    const imgArray = [firstImg, secongImg, thirdImg]; // an array of all the images of suggestions
-
-    const suggestionsSize = standardSize * suggestionsArray.size; // by default: standardSize = 100px
-    const imgSize = standardSize;
-
-    addedDiv.setAttribute('style', `height: ${suggestionsSize} px; margin: 10px 0px 0px 0px; display: flex; flex-direction: row;`);
-
-    const name = 'Kuumavalssattu%201D%20RST%20levy%2012.0x1500x3000mm'
-    fetch(`${HOST}/get_info?name=${name}`, {method: "GET"})
+    // console.log(name)name
+    // const name = 'Kuumavalssattu%201D%20RST%20levy%2012.0x1500x3000mm'
+    fetch(`${HOST}/get_info?name=${encodeURIComponent(name)}`, {method: "GET"})
         .then(res => res.json())
         .then(res => {
             const product_info = res[0]
@@ -47,69 +49,10 @@ if (firstDiv) {
             fetch(`${HOST}/get_alternatives?name=${name}`, {method: "GET"})
                 .then(res => res.json())
                 .then(res => {
-                    const alternatives = res
-                    console.log(res)
-
 
                     // ADDING SUGGESTIONS -------------------------------------------------------
-                    for (let i = 0; i < res.length; i++) {
-                        addedDiv.appendChild(suggestionsArray[i]); // To container for all 3 suggestions, add each suggestion div
-                        suggestionsArray[i].setAttribute('style', 'height: ' + imgSize + 'px; display: flex; flex-direction: row; overflow: hidden; margin-right: 10px;');
-
-                        const imgDiv = document.createElement("div"); // div for suggestion image
-                        const rightDiv = document.createElement("div"); // div for name, price, etc
-
-                        imgDiv.setAttribute('style', 'height: 100%;');
-                        rightDiv.setAttribute('style', 'display: flex; flex-direction: column;');
-
-                        suggestionsArray[i].appendChild(imgDiv);
-                        suggestionsArray[i].appendChild(rightDiv); // add the two divs to each suggestion
-
-                        imgDiv.appendChild(imgArray[i])
-                        imgArray[i].src = res[i]['ImageURL'];
-                        imgArray[i].setAttribute('style', 'padding: 5px 5px 5px 5px; max-height: 90px;');
-                
-
-
-
-
-                        // Description of the suggestion ----------------------------------------
-                        const namePriceDiv = document.createElement("div");
-                        const emissionsDiv = document.createElement("div");
-
-                        namePriceDiv.classList.add("namePrice");
-                        emissionsDiv.classList.add("emissions");
-
-                        rightDiv.appendChild(namePriceDiv);
-                        rightDiv.appendChild(emissionsDiv);
-
-                        namePriceDiv.setAttribute('style', 'height: ' + rightDiv.offsetHeight/2 + 'px; margin: 5px 5px 5px 5px;');
-                        namePriceDiv.innerHTML = res[i]['Name'];
-
-                        emissionsDiv.setAttribute('style', 'height: ' + rightDiv.offsetHeight/2 + 'px; margin: 0px 5px 5px 5px; text-align: center; display: flex; flex-direction: column;');
-                        const emissionsSpan = document.createElement("span");
-                        const priceSpan = document.createElement("span");
-                        emissionsDiv.appendChild(emissionsSpan);
-                        emissionsDiv.appendChild(priceSpan);
-                        emissionsSpan.setAttribute('style', 'background: lightgreen; display:inline-block;');
-                        emissionsSpan.innerHTML = `-${res[i]['Co2Em']} CO2e kg`;
-
-                        const productPrice = Number(product_info['Price'].slice(0,product_info['Price'].length-3));
-                        const altPrice = Number(res[i]['Price'].slice(0,res[i]['Price'].length-3));
-                        console.log("product price", productPrice)
-                        console.log("alt price", altPrice);
-
-
-                        if (productPrice > altPrice)
-                            priceSpan.innerHTML=`-${Math.round(Math.abs(altPrice-productPrice))}$`
-                        else 
-                            priceSpan.innerHTML=`+${Math.round(Math.abs(altPrice-productPrice))}$`
-
-                    }
+                    add_suggestions(res, suggestionsDiv, product_info)
                     // --------------------------------------------------------------------------------
-                    
-
-
 
 
                     // const tagNames = document.getElementsByClassName("ReferencePrice AlignLeft");
@@ -133,28 +76,82 @@ if (firstDiv) {
                     //     }
 
 
-
                     // }
 
 
 
+                    const productInfoDiv = document.getElementsByClassName("InfoArea New")[0];
 
+                    const susScore = document.createElement('h1')
+                    susScore.innerText = "134"
+                    productInfoDiv.insertBefore(susScore, productInfoDiv.children[2])
 
-
-                    const sustainabilityInfo = document.getElementsByClassName("InfoArea New");
-
-                    if (sustainabilityInfo) {
+                    if (productInfoDiv) {
                         const susDiv = document.createElement("div");
-                        sustainabilityInfo[0].appendChild(susDiv)
+                        productInfoDiv.appendChild(susDiv)
 
                         susDiv.setAttribute('style', 'background; red; height: 20px')
                         susDiv.innerHTML = 'hey';
-
-
-                            
-                        }
+                    }
                 })
         })
-    
 
+
+}
+
+function add_suggestions(res, suggestionsDiv, product_info) {
+    for (let i = 0; i < res.length; i++) {
+        const suggestionDiv = document.createElement("div");
+
+        suggestionsDiv.appendChild(suggestionDiv); // To container for all 3 suggestions, add each suggestion div
+        suggestionDiv.setAttribute('style', 'height: ' + imgSize + 'px; display: flex; flex-direction: row; overflow: hidden; margin-right: 10px;');
+
+        const imgDiv = document.createElement("div"); // div for suggestion image
+        const imgImg = document.createElement("img"); // suggestion image
+        const descriptionDiv = document.createElement("div"); // div for name, price, etc
+
+        imgDiv.setAttribute('style', 'height: 100%;');
+        descriptionDiv.setAttribute('style', 'display: flex; flex-direction: column;');
+
+        suggestionDiv.appendChild(imgDiv);
+        suggestionDiv.appendChild(descriptionDiv); // add the two divs to each suggestion
+
+        imgDiv.appendChild(imgImg)
+        imgImg.src = res[i]['ImageURL'];
+        imgImg.setAttribute('style', 'padding: 5px 5px 5px 5px; max-height: 90px;');
+
+
+        // Description of the suggestion ----------------------------------------
+        const namePriceDiv = document.createElement("div");
+        const emissionsDiv = document.createElement("div");
+
+        namePriceDiv.classList.add("namePrice");
+        emissionsDiv.classList.add("emissions");
+
+        descriptionDiv.appendChild(namePriceDiv);
+        descriptionDiv.appendChild(emissionsDiv);
+
+        namePriceDiv.setAttribute('style', 'height: ' + descriptionDiv.offsetHeight / 2 + 'px; margin: 5px 5px 5px 5px;');
+        namePriceDiv.innerHTML = res[i]['Name'];
+
+        emissionsDiv.setAttribute('style', 'height: ' + descriptionDiv.offsetHeight / 2 + 'px; margin: 0px 5px 5px 5px; text-align: center; display: flex; flex-direction: column;');
+        const emissionsSpan = document.createElement("span");
+        const priceSpan = document.createElement("span");
+        emissionsDiv.appendChild(emissionsSpan);
+        emissionsDiv.appendChild(priceSpan);
+        emissionsSpan.setAttribute('style', 'background: lightgreen; display:inline-block;');
+        emissionsSpan.innerHTML = `-${res[i]['Co2Em']} CO2e kg`;
+
+        const productPrice = Number(product_info['Price'].slice(0, product_info['Price'].length - 3));
+        const altPrice = Number(res[i]['Price'].slice(0, res[i]['Price'].length - 3));
+        console.log("product price", productPrice)
+        console.log("alt price", altPrice);
+
+
+        if (productPrice > altPrice)
+            priceSpan.innerHTML = `-${Math.round(Math.abs(altPrice - productPrice))}$`
+        else
+            priceSpan.innerHTML = `+${Math.round(Math.abs(altPrice - productPrice))}$`
+
+    }
 }
